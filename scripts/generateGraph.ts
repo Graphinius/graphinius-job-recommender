6362f98fb2ba735a694227c1cdc1e1bb94382b35
Bernd Malle <bernd.malle@gmail.com>
2019-09-10 18:54:41 +0200

    reading kaggle recommender graph (users, jobs, applications) including node features works - at ~9Gigs...

--- scripts/generateGraph.ts
+++ scripts/generateGraph.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as readline from 'readline';
 import * as uuid from 'uuid';
 import { ITypedEdge } from 'graphinius/lib/core/typed/TypedEdge';
 import { ITypedNode, TypedAdjSets } from 'graphinius/lib/core/typed/TypedNode';
@@ -13,43 +12,32 @@ const v4 = uuid.v4;
   const g = new TypedGraph('job recommender BIG');
 
   createUsers(g);
+  console.log(g.stats);
+  console.log(g.n('user_47'));
+
   createJobs(g);
+  console.log(g.stats);
+  console.log(g.n('job_169528'));
+
   createApps(g);
-
-  // console.log(g.stats);
-
+  console.log(g.stats);
 })();
 
 
 
 function createUsers(g: TypedGraph) {
   const users_columns = ['UserID', 'WindowID', 'Split', 'City', 'State', 'Country', 'ZipCode', 'DegreeType', 'Major', 'GraduationDate', 'WorkHistoryCount', 'TotalYearsExperience', 'CurrentlyEmployed', 'ManagedOthers', 'ManagedHowMany'];
-  // const first1KUsersFile = path.join(__dirname, '../data/first_1K_users.tsv');
   const allUsersFile = path.join(__dirname, '../data/original/users.tsv');
 
-  const rl = readline.createInterface({
-    input: fs.createReadStream(allUsersFile) // first1KUsersFile
-  });
-
-  let line_no = 0;
-
-  // event is emitted after each line
-  rl.on('line', line => {
-    if (line_no++ === 1) {
-      return;
-    }
+  const file = fs.readFileSync(allUsersFile).toString().split('\n');
+  file.shift();
+  file.forEach(line => {
     const line_arr = line.split('\t');
-    
-    const node = g.addNodeByID('user_'+line_arr[0], { type: 'User' });
-    for (let i = 1; i < users_columns.length; i++) {
-      node.setFeature(users_columns[i], line_arr[users_columns[i]]);
-    }
-  });
 
-  // end
-  rl.on('close', function (line) {
-    console.log(g.stats);
-    console.log('Total lines read : ' + line_no);
+    const node = g.addNodeByID('user_' + line_arr[0], { type: 'User' });
+    for (let i = 1; i < users_columns.length; i++) {
+      node.setFeature(users_columns[i], line_arr[i]);
+    }
   });
 }
 
@@ -57,76 +45,64 @@ function createUsers(g: TypedGraph) {
 
 function createJobs(g: TypedGraph) {
   const jobs_columns = ['JobID', 'WindowID', 'Title', 'Description', 'Requirements', 'City', 'State', 'Country', 'Zip5', 'StartDate', 'EndDate'];
-  const allJobsFile = path.join(__dirname, `../data/original/jobs.tsv`);
+  const splitFiles = [];
+  [1, 2, 3, 4, 5, 6, 7].forEach(sf => {
+    splitFiles.push(path.join(__dirname, `../data/original/splitjobs/jobs${sf}.tsv`));
+  });
 
   const jobIDs = {};
+  let line_arr = [];
+  let jid = null;
+  let node;
 
-  const rl = readline.createInterface({
-    input: fs.createReadStream(allJobsFile)
-  });
+  for (let sf of splitFiles) {
 
+    const file = fs.readFileSync(sf).toString().split('\n');
+    file.shift();
+    file.forEach(line => {
+      line_arr = line.split('\t');
 
-  let line_no = 0;
+      jid = 'job_' + line_arr[0];
+      // console.log(jid);
+      if (jobIDs[jid]) {
+        return;
+      }
 
-  // event is emitted after each line
-  rl.on('line', line => {
-    if (line_no++ === 1) {
-      return;
-    }
-    const line_arr = line.split('\t');
-    
-    const jid = 'jobs_' + line_arr[0];
-    // console.log(jid);
-    if ( jobIDs[jid] ) {
-      return;
-    }
+      node = g.addNodeByID(jid, { type: 'Job' });
+      for (let i = 1; i < jobs_columns.length; i++) {
+        node.setFeature(jobs_columns[i], line_arr[i]);
+      }
+      jobIDs[jid] = true;
+    });
 
-    const node = g.addNodeByID(jid, { type: 'Job' });
-    for (let i = 1; i < jobs_columns.length; i++) {
-      node.setFeature(jobs_columns[i], line_arr[jobs_columns[i]]);
-    }
-    jobIDs[jid] = true;
-  });
-
-
-  // end
-  rl.on('close', function (line) {
     console.log(g.stats);
-    console.log('Total lines read : ' + line_no);
-  });
-
+  }
 }
 
 
 
 function createApps(g: TypedGraph) {
-  const apps_columns = ['UserID',	'WindowID',	'Split',	'ApplicationDate',	'JobID'];
+  const apps_columns = ['UserID', 'WindowID', 'Split', 'ApplicationDate', 'JobID'];
   const allAppsFile = path.join(__dirname, `../data/original/apps.tsv`);
+  let line_arr = [], a, b;
+  let nr_edges = 0;
+  
+  const file = fs.readFileSync(allAppsFile).toString().split('\n');
+  file.shift();
+  file.forEach(line => {
+    line_arr = line.split('\t');
+    // console.log(line_arr);
+    a = g.n('user_' + line_arr[0]);
+    // last entry in file has an additional '\r' attached...
+    b = g.n('job_' + line_arr[line_arr.length - 1].replace('\r', ''));
 
-  const rl = readline.createInterface({
-    input: fs.createReadStream(allAppsFile)
-  });
-
-
-  let line_no = 0;
-
-  // event is emitted after each line
-  rl.on('line', line => {
-    if (line_no++ === 1) {
-      return;
+    if ( a && b ) {
+      // console.log(`Adding edge between user_${line_arr[0]} and job_${line_arr[line_arr.length - 1]}`);
+      g.addEdgeByID(v4(), a, b, { type: 'APPLIED_TO' });
+      if ( ++nr_edges % 10000 === 0 ) {
+        console.log('added edges: ', nr_edges);
+      }
     }
-    const line_arr = line.split('\t');
-    
-    const node = g.addEdgeByNodeIDs(v4(), line_arr[0], line_arr[line_arr.length-1],  { type: 'APPLIED_TO' });
-    // for (let i = 1; i < apps_columns.length; i++) {
-    //   node.setFeature(apps_columns[i], line_arr[apps_columns[i]]);
-    // }
-  });
-
-
-  // end
-  rl.on('close', function (line) {
-    console.log(g.stats);
-    console.log('Total lines read : ' + line_no);
   });
 }
+
